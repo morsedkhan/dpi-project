d258e3431aedfb7251f105b6bd92d7214d7e7853
masudrana08 <rana028511@gmail.com>
2021-02-28 15:15:33 +0600

    basic dialoug implementation

---
+++ src/components/home/newsDialoug/NewsDialoug.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+
+
+
+ function NewsDialoug(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div  onClick={handleClickOpen}>
+        {props.children}
+      </div>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        {props.children}
+      </Dialog>
+    </>
+  );
+}
+export default NewsDialoug
