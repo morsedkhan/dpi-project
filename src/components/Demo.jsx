6aac9d631e812ed50a5595c540207cc595fdb41f
morshadKhan <61825151+morsedkhan@users.noreply.github.com>
2021-03-10 14:48:52 +0600

    adding some image

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -11,6 +11,10 @@ const Demo = () => {
             <h1>This is demo component</h1>
             <h1>This is demo component</h1>
             <h1>This is demo component</h1>
+            <h1>This is demo component</h1>
+            <h1>This is demo component</h1>
+            <h1>This is demo component</h1>
+            <h1>This is demo component</h1>
         </div>
     );
 };
